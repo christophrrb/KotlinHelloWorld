c8d5bcf7ad396b916a15c3f1c098de7d4c8ba894
Christopher <christopherballenger@gmail.com>
2017-08-07 23:09:05 -0400

    The text in the text field now gets transferred to the second activity! There is also an on-click listener in the code now. It's on @+id/button4.

--- app/src/main/java/com/example/christophrrb/kotlinhelloworld/MainActivity.kt
+++ app/src/main/java/com/example/christophrrb/kotlinhelloworld/MainActivity.kt
@@ -4,20 +4,27 @@ package com.example.christophrrb.kotlinhelloworld
 import android.os.Bundle
 import android.app.Activity
  import android.content.Context
+ import android.content.Intent
  import android.graphics.drawable.Drawable
  import android.view.View
  import android.widget.ImageView
  import android.widget.Toast
  import com.example.christophrrb.kotlinhelloworld.R
 import kotlinx.android.synthetic.main.activity_main.*
- import org.jetbrains.anko.toast
+ import org.jetbrains.anko.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        imageView.resources.getDrawable(R.mipmap.atlanta_jazz_festival_selfie);
+        imageView.resources.getDrawable(R.drawable.atlanta_jazz_festival_selfie);
+
+        button4.setOnClickListener {
+            val intent = Intent(this@MainActivity, SecondActivity::class.java)
+            intent.putExtra("dato", nombre.text)
+            startActivity(intent)
+        }
     }
 
     fun cambiarTexto(v: View) {
@@ -43,4 +50,14 @@ class MainActivity : AppCompatActivity() {
     fun toast(v: View) {
         toast("Tastes so good.");
     }
+
+//    companion object {
+//
+//        fun segundaActividad(nombre: String, context: Context): Intent {
+//            var dato: String = nombre;
+//            val intent: Intent = Intent(context, SecondActivity::class.java);
+//            intent.putExtra("nombre", nombre);
+//            return intent;
+//        }
+//    }
 }
--- app/src/main/java/com/example/christophrrb/kotlinhelloworld/SecondActivity.kt
+++ app/src/main/java/com/example/christophrrb/kotlinhelloworld/SecondActivity.kt
@@ -3,12 +3,19 @@ package com.example.christophrrb.kotlinhelloworld
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.app.Activity
+import android.content.Intent
 import com.example.christophrrb.kotlinhelloworld.R
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val intent: Intent = getIntent();
+        var bundle: Bundle = intent.getExtras();
+
+        textView2.text = bundle.get("dato").toString() ?: "Couldn't receive text from previous activity."
     }
 }
