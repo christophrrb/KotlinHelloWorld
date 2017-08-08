c8c8eccd9988757c6bfbbe0e38ae9ed64b5a26fe
Christopher <christopherballenger@gmail.com>
2017-08-08 00:41:42 -0400

    Can you say radio buttons! Glory to God. Also, there is a button that enables itself once one of the radio buttons is checked, but as it says, it doesn't do anything... yet 🙂.

--- app/src/main/java/com/example/christophrrb/kotlinhelloworld/SecondActivity.kt
+++ app/src/main/java/com/example/christophrrb/kotlinhelloworld/SecondActivity.kt
@@ -4,8 +4,10 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.app.Activity
 import android.content.Intent
+import android.widget.RadioGroup
 import com.example.christophrrb.kotlinhelloworld.R
 import kotlinx.android.synthetic.main.activity_second.*
+import org.jetbrains.anko.*
 
 class SecondActivity : AppCompatActivity() {
 
@@ -16,6 +18,33 @@ class SecondActivity : AppCompatActivity() {
         val intent: Intent = getIntent();
         var bundle: Bundle = intent.getExtras();
 
-        textView2.text = bundle.get("dato").toString() ?: "Couldn't receive text from previous activity."
+        textView2.text = bundle.get("dato").toString() ?: "Couldn't receive text from previous activity.";
+
+//        radioGroup.setOnCheckedChangeListener(object: RadioGroup.OnCheckedChangeListener() {
+//          fun onCheckedChange(group: RadioGroup, checkedId: Int) {
+//              if (checkedId == 1) {
+//                toast("Bananas!");
+//              } else if (checkedId == 2){
+//                    toast("Pears!");
+//              } else {
+//                    toast("Mangoes!");
+//              }
+//          }
+//        })
+
+        radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            button5.setEnabled(true);
+            if (checkedId == R.id.bananas)  {
+                toast("Bananas!");
+            } else if (checkedId == R.id.pears){
+                toast("Pears!");
+            } else {
+                toast("Mangoes!")
+            }
+        }
+
+        button5.setOnClickListener {
+            toast("I don't do anything yet. \uD83D\uDE42");
+        }
     }
 }
